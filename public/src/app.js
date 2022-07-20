55dfef1aa9063178f705a42190122410be121869
Jonathan McGrath <you@example.com>
2022-07-19 18:27:28 -0700

    Added the app file, app is being rendered

---
+++ public/src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+/*
+Will need to set up the routers and the routes here.App
+
+Also need to create the Navigation Bar and the Footer.
+
+*/
+
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello, world, from the App!</h1>
+    </div>
+  );
+}
+
--- public/src/index.js
+++ public/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {App} from './app.js';
+
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<h1>Hello, world!</h1>);
+root.render(<App />);
 console.log("Whatever");
