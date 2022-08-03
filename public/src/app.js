91995bd63766981184214cb50a30b3b19430bf42
Jonathan McGrath <you@example.com>
2022-08-03 12:21:36 -0700

    Adding cards search and sort

--- public/src/app.js
+++ public/src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import arkhamcards from './arkhamcards.json';
+import './app.css';
 
 
 /*
@@ -11,9 +13,23 @@ Also need to create the Navigation Bar and the Footer.
 
 
 export function App() {
+
+  const cards = arkhamcards.filter( (card) => card.pack_name === 'Core Set' );
+
+  const carddiv = cards.map( (card) => {
+    return <div key = {card.name+card.imagesrc}>
+     
+      <img width = "auto" height = "250px" src = {"https://arkhamdb.com"+card.imagesrc}></img>
+     
+      </div>
+      });
+
+  
   return (
     <div>
       <h1>Hello, world, from the App!</h1>
+      <div className = "cards">{carddiv}</div>
+      {/* /* <div>{JSON.stringify(cards)}</div> */}
     </div>
   );
 }
